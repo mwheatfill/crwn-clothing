30b476df2e8bd435d1b2451fafb8d95c690cb2b6
Michael Wheatfill <michaelw@microsoft.com>
2021-05-31 12:32:57 -0700

    create smaller components for home page, finished stying homepage

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
+
+class Directory extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://rb.gy/jljaxc",
+          id: 1,
+          linkUrl: "shop/hats",
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://rb.gy/gipqw6",
+          id: 2,
+          linkUrl: "shop/jackets",
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://rb.gy/b6sqij",
+          id: 3,
+          linkUrl: "shop/sneakers",
+        },
+        {
+          title: "womens",
+          imageUrl: "https://rb.gy/58pd4h",
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://rb.gy/zt9x6t",
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
